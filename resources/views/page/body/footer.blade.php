978ec681500d01a9752d13aeb4996d5a821339e2
Tombrown1 <godwintombrown@gmail.com>
2022-03-23 17:08:34 +0100

    Change

--- resources/views/page/body/footer.blade.php
+++ resources/views/page/body/footer.blade.php
@@ -39,7 +39,7 @@
                   Do you have need for recorded messages or suggestion that you think that will add values and improves our operation, feel free to visit the unit office and your opinion will be helpful.
                 </p>
                 <div class="footer-contacts">
-                  <p><span>Tel:</span> +234 806 1539</p>
+                  <p><span>Tel:</span> +234 806 907 1539</p>
                   <p><span>Email:</span> godwintombrown@gmail.com</p>
                   <p><span>Sunday Service:</span> 6am-2pm</p>
                   <p><span>Wednesday Service Hour:</span> 5pm-6:30pm</p>
@@ -52,15 +52,26 @@
           <div class="col-md-4">
             <div class="footer-content">
               <div class="footer-head">
-                <h4>Instagram</h4>
-                <div class="flicker-img">
+                <h4>Contact</h4>
+                <p>
+                  As a unit, our unit church office is always open during church service,
+                  sundays and wed. midweek communion service.
+                </p>
+                <div class="footer-contacts">
+                  <p><span>Address</span>#4 Kaduna Street D/Line, Port Harcourt</p>
+                  <p><span>Email:</span> godwintombrown@gmail.com</p>
+                  <p><span>Sunday Service:</span> 6am-2pm</p>
+                  <p><span>Wednesday Service Hour:</span> 5pm-6:30pm</p>
+                  <p><span>Covenant Service Hour:</span> 6am-7am</p>
+                </div>
+                <!-- <div class="flicker-img">
                   <a href="#"><img src="assets/img/portfolio/1.jpg" alt=""></a>
                   <a href="#"><img src="assets/img/portfolio/2.jpg" alt=""></a>
                   <a href="#"><img src="assets/img/portfolio/3.jpg" alt=""></a>
                   <a href="#"><img src="assets/img/portfolio/4.jpg" alt=""></a>
                   <a href="#"><img src="assets/img/portfolio/5.jpg" alt=""></a>
                   <a href="#"><img src="assets/img/portfolio/6.jpg" alt=""></a>
-                </div>
+                </div> -->
               </div>
             </div>
           </div>
